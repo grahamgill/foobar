1b885f80a1d8e4426f04ecf6c1d6ab30d26d7bf1
Graham Gill <graham.gill@gmail.com>
2022-05-05 22:05:14 -0400

    shrink xor calc plan

--- src/xorqueue.py
+++ src/xorqueue.py
@@ -34,6 +34,34 @@ def xorqueue(start, length):
     Verifier complains about solution with list comprehension, so reworking to eliminate it and
     replace by a nested for loop. List comprehension could lead to very big and/or slow list, but
     we don't need to store a list, we just need to accumulate them with xor.
+
+    OK none of the improvements and checks I've made has solved the problem of the three failing  
+    test cases. There's no error in the logic of the indices. Possibly passing in a float that is equal 
+    to an integer, like `1.0 == 1`, isn't happening. The queue ending early because worker MAX_WORKER_ID
+    occurs in the middle of the matrix (and the matrix is always sorted in order with no gaps) isn't happening.
+
+    If there's no logic problem and no input problem, then the problem must be space or time. We took care of the
+    space problem by eliminating the construction of the list, but that didn't solve the problem.
+
+    So we're left with a time problem. Potentially we're xor-ing together hundreds of millions of integers. We need
+    to look for properties which will allow us to speed things up.
+
+    `x ^ x == 0` is a useful one. So ^ is self-cancelling (idempotent).
+
+    ^ is also commutative, associative, and 0 is the identity of the operation. This allows `x ^ y ^ x == y`, and so
+    `1 ^ ... ^ y == 1 ^ ... ^ x ^ (x + 1) ^ ... ^ y`, which allows `(1 ^ ... ^ x) ^ (1 ^ ... ^ y) == (x + 1) ^ ... ^ y`. 
+    We can also write `1 ^ ... ^ y == 0 ^ 1 ^ ... ^ y`. But all this isn't enough to shorten the number of consecutive
+    numbers we may need to xor. There are still millions.
+
+    But now there's a nice property of xor such that sequences of length 4 starting from a multiple of 4 xor together to 0,
+    i.e. `4n ^ (4n + 1) ^ (4n + 2) ^ (4n + 3) == 0`. So, then if `y = 4n + k`, with `0 <= k <= 3`, we have
+    `0 ^ ... ^ y ==` 
+        * `4n`, if `k == 0`;
+        * `4n ^ (4n + 1) == 1`, if `k == 1`;
+        * `4n ^ (4n + 1) ^ (4n + 2) == 4n + 3`, if `k == 2`;
+        * `4n ^ (4n + 1) ^ (4n + 2) ^ (4n + 3) == 0`, if `k == 3`.
+
+    Putting these properties together we can dramatically shrink the number of xor computations we need.
     """
 
     assert start == int(start), "worker ID must be integral, but got " + repr(start)
@@ -45,10 +73,10 @@ def xorqueue(start, length):
         repr(start)
     assert length >= 1, "length promised to be positive, but got " + \
         repr(length)
-    # assert start - 1 + length**2 <= MAX_WORKER_ID, """all worker IDs promised to lie between """ + \
-    #     repr(MIN_WORKER_ID) + """ and """ + repr(MAX_WORKER_ID) + """ inclusive, and be strictly
-    #     increasing with no gaps, but the implied final ID at checkpoint is """ + \
-    #     repr(start - 1 + length**2)
+    assert start - 1 + length**2 <= MAX_WORKER_ID, """all worker IDs promised to lie between """ + \
+        repr(MIN_WORKER_ID) + """ and """ + repr(MAX_WORKER_ID) + """ inclusive, and be strictly
+        increasing with no gaps, but the implied final ID at checkpoint is """ + \
+        repr(start - 1 + length**2)
 
     checksum = 0
     for j in range(0, length):
